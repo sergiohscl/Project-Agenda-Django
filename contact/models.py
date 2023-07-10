470168b88e9b9f9d29fcfad3050fb09a6e66f7e5
sergiohscl <sergiohscl@gmail.com>
2023-07-10 08:35:28 -0300

    Model Meta options, verbose_name

--- contact/models.py
+++ contact/models.py
@@ -10,10 +10,15 @@ from django.utils import timezone
 # owner (foreign key)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return self.name
+    
     
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
