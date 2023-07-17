d45f249164d1b430caccba9970954c4b14558f4c
sergiohscl <sergiohscl@gmail.com>
2023-07-17 08:47:12 -0300

    Creating a Dynamic Form with Django's forms.ModelForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
-
+        'form': ContactForm()
     }
-
-    print()
-    print(request.method)
-    print()
 
     return render(
         request,
