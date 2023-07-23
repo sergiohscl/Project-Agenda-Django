2accc0c1678362b03f84080a77bc3d7606145f50
sergiohscl <sergiohscl@gmail.com>
2023-07-23 10:39:38 -0300

    Creating update and delete

--- contact/forms.py
+++ contact/forms.py
@@ -50,8 +50,8 @@ class ContactForm(forms.ModelForm):
             )
             self.add_error('first_name', msg)
             self.add_error('last_name', msg)
-               
-            return super().clean()
+
+        return super().clean()
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
--- contact/urls.py
+++ contact/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
